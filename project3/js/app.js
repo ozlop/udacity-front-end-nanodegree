90526e3521fa52c62e5bc229a68b9b4cb7d7f419
ozlop <oswaldo.t.lopez@gmail.com>
2019-05-29 22:36:16 -0700

    feat: Add Player basic movement functionality
    The player's movement is handled after a keyup is event is captured and
    player position is modified accordingly. Game map coundaries were set
    for the player class to prevent the character from travelling off the
    rendered game map.

--- project3/js/app.js
+++ project3/js/app.js
@@ -24,12 +24,75 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+const Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
 
+    // Starting player position
+    this.x = 200;
+    this.y = 375;
+
+    // Player boundary variables
+    this.xBoundaryMin = 0;
+    this.xBoundaryMax = 400;
+    this.yBoundaryMin = -50;
+    this.yBoundaryMax = 375;
+
+};
+
+Player.prototype.update = function(x=0, y=0) {
+    this.x += x;
+    this.y += y;
+    console.log(this.x, this.y)
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(event) {
+    const yMove = 85;
+    const xMove = 100;
+
+    switch(event) {
+        case 'up':
+            if (this.y > this.yBoundaryMin) {
+                this.update(0, -yMove);
+            }
+
+            break;
+
+        case 'down':
+            if (this.y < this.yBoundaryMax) {
+                this.update(0, yMove);
+            }
+
+            break;
+
+        case 'left':
+            if (this.x > this.xBoundaryMin) {
+                this.update(-xMove);
+            }
+
+            break;
+
+        case 'right':
+            if (this.x < this.xBoundaryMax) {
+                this.update(xMove);
+            }
+    }
+
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+const player = new Player();
+const allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your
