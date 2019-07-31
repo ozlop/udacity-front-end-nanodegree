0e0b340194f2ee8c6c868611057868dd15bd855e
ozlop <oswaldo.t.lopez@gmail.com>
2019-07-30 21:38:52 -0700

    feat: Add semantic tag to elements generated in JS
    Switched the H1 semantic tag to an H2 in the restaurant-name element
    created in the JS code for the restaurant list content.

--- project5/js/main.js
+++ project5/js/main.js
@@ -163,10 +163,11 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + " Restaurant"
   image.tabIndex = 0;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   name.tabIndex = 0;
   li.append(name);
