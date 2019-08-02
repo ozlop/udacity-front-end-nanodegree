d5932c09e71204ccb05484c70e12f0c1ace53627
ozlop <oswaldo.t.lopez@gmail.com>
2019-08-01 21:19:35 -0700

    feat: Add site assets to service worker cache
    Added the remaining site assets to the service worker in order to cache
    ALL content. Restaurant details pages that have been visited at least
    once will fully render if the user is offline. If the restaurant detail
    page was not fully cached, it will render while offline but will not
    have a mapbox interactive map.

--- project5/sw.js
+++ project5/sw.js
@@ -1,7 +1,26 @@
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open('restaurant-static').then(function(cache) {
-            return cache.add('/')
+            return cache.addAll([
+                '/',
+                'index.html',
+                'restaurant.html',
+                'css/styles.css',
+                'data/restaurants.json',
+                'js/dbhelper.js',
+                'js/main.js',
+                'js/restaurant_info.js',
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+                'img/5.jpg',
+                'img/6.jpg',
+                'img/7.jpg',
+                'img/8.jpg',
+                'img/9.jpg',
+                'img/10.jpg',
+            ]);
         })
     )
 });
